ad54f583099f64a6bd7c99537bd8f84765cfb968
Nagima Rakhimova <nagimenok88@gmail.com>
2020-10-01 13:43:01 -0400

    changes to clock.js

--- clock.js
+++ clock.js
@@ -1,15 +1,15 @@
 function displayTime() {
     var dTime = new Date();
-    var hour = now.getHours();
-    var minute = now.getMinutes();
-    var seconds = now.getSeconds();
+    var hour = dTime.getHours();
+    var minute = dTime.getMinutes();
+    var second = dTime.getSeconds();
    
   var session = (hour < 12) ? "AM" : "PM";
     
-   hour = (hour > 12) ? hours - 12 : hours;
+   hour = (hour > 12) ? hour - 12 : hour;
 
 
-    document.getElementById('clock').innerHTML = hour + " : " + minutes + " : " + seconds + " " + session;
-    var t = setTimeout(displayTime, 1000);
+    document.getElementById('clock').innerHTML = hour + " : " + minute + " : " + second + " " + session;
+    var time = setTimeout(displayTime, 1000);
     
 }
