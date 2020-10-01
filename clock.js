31bdc26dff5e6e09793cdc164ef39691d0be49f7
Nagima Rakhimova <nagimenok88@gmail.com>
2020-10-01 19:44:08 -0400

    modified

--- clock.js
+++ clock.js
@@ -3,13 +3,48 @@ function displayTime() {
     var hour = dTime.getHours();
     var minute = dTime.getMinutes();
     var second = dTime.getSeconds();
-    var date = dTime.getFullYear()+'-'+dTime.getMonth()+'-'+dTime.getDate();
+    // var date = dTime.getFullYear()+'-'+dTime.getMonth()+'-'+dTime.getDate();
+    var date = dTime.getDay()
+    var year = dTime.getFullYear()
    
 
   var session = (hour < 12) ? "AM" : "PM";
   hour = (hour > 12) ? hour - 12 : hour;
 
-  	document.getElementById('date').innerHTML = date;
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+]
+
+const monthIndex = dTime.getMonth()
+const monthName = months[monthIndex]
+console.log(monthName)
+
+const weekdays = [
+'Monday', 
+'Tuesday', 
+'Wednesday', 
+'Thursday', 
+'Friday', 
+'Saturday', 
+'Sunday'
+]
+
+const dateIndex = dTime.getDay();
+const dateName = weekdays[dateIndex];
+console.log(dateName);
+
+  	document.getElementById('date').innerHTML = dateName + ", " + monthName + " " + date + ", " + year;
     document.getElementById('clock').innerHTML = hour + " : " + minute + " : " + second + " " + session;
     var t = setTimeout(displayTime, 1000);
     
